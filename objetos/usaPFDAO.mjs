bcb6d4d1ac4e2d26d86d72013d3ff52a0cca7756
thamihllo <thamihllo@gmail.com>
2025-09-29 22:40:23 -0300

    Update usaPFDAO.mjs

--- objetos/usaPFDAO.mjs
+++ objetos/usaPFDAO.mjs
@@ -6,18 +6,18 @@ import Endereco from './pessoas/Endereco.js';
 import Telefone from './pessoas/Telefone.js'; 
 
 const pf = new PF();
-pf.setNome('Maria');
-pf.setEmail('maria@ifb.edu.br');
-pf.setCPF('111.222.333-44');
+pf.setNome('Gustavo');
+pf.setEmail('Gustavo@ifb.edu.br');
+pf.setCPF('234.567.888-99');
 
 const endPF = new Endereco();
-endPF.setLogradouro('SQN 300');
-endPF.setCep('98765-432');
+endPF.setLogradouro('QNM 30');
+endPF.setCep('98395-6280');
 pf.setEndereco(endPF);
 
 const fonePF = new Telefone();
 fonePF.setDdd('61');
-fonePF.setNumero('98765-4321');
+fonePF.setNumero('99923-3427');
 pf.addTelefone(fonePF);
 
 const pfdao = new PFDAO(pf);
